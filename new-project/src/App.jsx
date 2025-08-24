6d84d32908fc4f1a6eff7c326cfec9d08d44cf60
tkdals1144 <146908349+tkdals1144@users.noreply.github.com>
2025-08-24 20:26:48 +0900

    signup page
    update signup page

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Login from './Login.jsx'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1>hi</h1>
+      <Login/>
     </>
   )
 }
--- new-project/src/Login.jsx
+++ new-project/src/Login.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { axios } from "axios";
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
 
 
 function Login({ setUser }) {
